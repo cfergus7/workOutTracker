967d6cd12b935ac9b4945d1da0709af108ba763c
Chris Ferguson <cfergus7@gmail.com>
2020-07-13 23:21:09 -0500

    brick by brick

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("public"))
+
+mongoose.connect("mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+})
+
+app.use(require("./routes/api.js"))
+
+app.use(require("./routes/views.js"))
+
+app.listen(3000, () => {
+    console.log("app is running on port 3000")
+})

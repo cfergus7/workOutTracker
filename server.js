e32bb7bec210d7a4887122fac778fd807d6700fa
Chris Ferguson <cfergus7@gmail.com>
2020-07-14 21:48:35 -0500

    lalala

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const logger = require("morgan");
-// define port
-const PORT = process.env.PORT || 3002;
-// create express app
+const express = require("express");
+const mongoose = require("mongoose");
+//define port
+const PORT = process.env.PORT || 3000
+
 const app = express();
-
-// set up morgan
-app.use(logger('dev'))
-
 // set express to be able to use json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // serve public files
-app.use(express.static('public'));
-
-
+app.use(express.static("public"));
 // set up mongoose database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
-// require routes
-require('./routes/api-routes.js')(app);
-require('./routes/html-routes.js')(app);
-
+// routes
+app.use(require("./routes/api.js"));
 // start app
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`);
+    console.log(`App running on port ${PORT}!`);
 });
